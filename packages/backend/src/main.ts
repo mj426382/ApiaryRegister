1da2ea64fb4b415d994ff651d1c44f7b9a60e6b2
mj426382 <mistrzumati@spoko.pl>
2022-06-20 09:28:23 +0200

    Update monorepo settings

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,11 +1,12 @@
 import { build } from './server'
 
 const PORT = parseInt(process.env.PORT ?? '8080')
+const ADDRESS = process.env.ADDRESS
 
 const startServer = async () => {
   const server = await build()
 
-  server.listen({ port: PORT }, (err, address) => {
+  server.listen({ port: PORT, host: ADDRESS }, (err, address) => {
     if (err) {
       console.error(err)
       process.exit(1)
